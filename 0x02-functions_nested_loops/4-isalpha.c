ed464a808c078065d0e369ee6571bba4b51a1cf4
Asarebea <asaredeborah287@gmail.com>
2023-04-25 17:09:29 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _isalpha - Entry point
+  * Description - A function that checks for alphabetic character
+  * @c: Chcking character from ASCII table
+  * Return: 1 for lower or upper case letters, 0 for others
+  */
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
