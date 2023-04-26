5297ec59e002cc74764a8e3cdffa0edae8b191c5
Asarebea <asaredeborah287@gmail.com>
2023-04-25 20:12:59 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * Description - Add two integers
+ * @i1: First integer
+ * @i2: Second integer
+ * Return: Result of integer sum
+ */
+
+int add(int i1, int i2)
+{
+	return (i1 + i2);
+}
+
+
