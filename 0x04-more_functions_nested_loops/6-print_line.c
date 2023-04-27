2e74aa13fb2a1df4c937f1215bfe308b42f70df5
Asarebea <asaredeborah287@gmail.com>
2023-04-27 13:41:32 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * print_line - Entry point
+  * Description - Print lines
+  * @n: number of times
+  * Return: void (success)
+  */
+
+void print_line(int n)
+{
+	int k;
+
+	for (k = 0; k < n; k++)
+	{
+		_putchar('_');
+	}
+		_putchar('\n');
+}
