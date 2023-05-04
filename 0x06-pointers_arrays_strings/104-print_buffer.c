cc7f196ee160c9a6c9278f93460595ba18b821c9
Asarebea <asaredeborah287@gmail.com>
2023-05-04 15:53:30 -0700

    Task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
-  * print_buffer - prints buffer
+  * print_buffer - prints  buffer
   * @b: buffer
   * @size: size of buffer
   * Return: no return
@@ -12,33 +13,32 @@ void print_buffer(char *b, int size)
 	int m, n, o;
 
 	if (size <= 0)
-		printf("\n");
+	printf("\n");
 	else
 	{
 	for (m = 0; m < size; m += 10)
 	{
-		printf("%.8x:", m);
-		for (n = m; n < m + 10; n++)
-		{
-		if (n % 2 == 0)
-		printf(" ");
-		if (n < size)
-		printf("%.2x", *(b + n));
-		else
-		printf("  ");
-		}
-		printf(" ");
-		for (o = m; o < m + 10; o++)
-		{
-		if (o >= size)
-			break;
-		if (*(b + o) < 32 || *(b + o) > 126)
-		printf("%c", '.');
-		else
-		printf("%c", *(b + o));
-		}
-		printf("\n");
+	printf("%.8x:", m);
+	for (n = m; n < m + 10; n++)
+	{
+	if (n % 2 == 0)
+	printf(" ");
+	if (n < size)
+	printf("%.2x", *(b + n));
+	else
+	printf("  ");
+	}
+	printf(" ");
+	for (o = m; o < m + 10; o++)
+	{
+	if (o >= size)
+	break;
+	if (*(b + o) < 32 || *(b + o) > 126)
+	printf("%c", '.');
+	else
+	printf("%c", *(b + o));
+	}
+	printf("\n");
 	}
 	}
 }
-
