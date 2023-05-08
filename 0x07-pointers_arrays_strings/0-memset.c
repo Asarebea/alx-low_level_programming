bbc689709ed3465dd5767069f687be13a6cf674a
Asarebea <asaredeborah287@gmail.com>
2023-05-08 13:38:04 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * _memset - Copies characters to the string pointed to.
+  * @b: the address of memory to print
+  * @n: number of bytes to be set
+  * @s: the size of the memory to print
+  * Return: nothing
+  */
+
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int k;
+
+	for (k = 0; s[k] < n; k++)
+	{
+	s[k] = b;
+	}
+	return (s);
+}
