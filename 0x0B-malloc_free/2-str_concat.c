0c53a179ae46b56456a46a890a6cadd397ad243d
Asarebea <asaredeborah287@gmail.com>
2023-05-18 09:40:16 -0700

    Task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - Entry point
+ * Description: concatenates two strings
+ * @s1: first string to concatenate
+ * @s2: second string to concatenate
+ * Return: strings together
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *deb;
+	int i, k;
+
+	if (s1 == NULL)
+	{
+	s1 = "";
+	}
+	if (s2 == NULL)
+	{
+	s2 = "";
+	}
+	i = k = 0;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+	for (k = 0; s2[k] != '\0'; k++)
+	{
+	deb = malloc(sizeof(char) * (i + k + 1));
+
+	if (deb == NULL)
+	{
+	return (NULL);
+	}
+	}
+	}
+	i = k = 0;
+
+	while (s1[i] != '\0')
+	{
+	deb[i] = s1[i];
+	i++;
+	}
+	while (s2[k] != '\0')
+	{
+		deb[i] = s2[k];
+		i++, k++;
+	}
+	deb[i] = '\0';
+	return (deb);
+}
